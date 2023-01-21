a7dd06158d565763d112867325807a79dfb8d71a
zick2 <34427350+zick2@users.noreply.github.com>
2023-01-21 17:56:19 -0600

    adding example 1, 2 and 3

---
+++ trpl_examples/01_basic_triangle_movel_example/triangle.py
@@ -0,0 +1,23 @@
+'''
+Example 1.0: movel() and pose()/p[]
+In this exmaple we are going to draw a triangle with the helpe of two functions Pose()/p[] and movel()
+'''
+
+from robot_command.rpl import *
+
+
+#NOTE: alays rember to set your units
+set_units("mm", "deg")
+
+def main():
+    waypoint_1 = p[0, 0, 0, 0, 0, 0]  # Creating pose waypoints using the pose factory short cut, p[] 
+    waypoint_3 = Pose(150, 0, 0, 0, 0, 0) # We can also create pose waypoints using Pose().
+    waypoint_2 = Pose(y = 100) # Using Pose() we can set specific parametars, the rest will default to zero.
+   
+    #Notes: movel() will make the robot's end effector to move linearly between points.
+    movel(waypoint_1) #move to waypoint_1
+    movel(waypoint_2, velocity=60, accel_scale = 1) # move to waypoint_2 with velocity at 60 and acceleration(0 - 1) at 1 or 100% 
+    movel(waypoint_3, velocity=1000, duration=5) # move to waypoint_3 with velocity at 1000 and within 5 sec
+    movel(waypoint_1, velocity=1000, strict_limits=True)  # move to waypoint_3 with velocity at 1000 and within 5 sec
+    
+    exit() # After we are done we exit/stop the main() loop
---
+++ trpl_examples/03_triangles_in_a_circle_movel_example/triangles_in_a_circle.py
@@ -0,0 +1,69 @@
+'''
+Example one: movel() and pose()/p[]
+'''
+
+from robot_command.rpl import *
+import math 
+
+set_units("mm", "deg")
+
+
+def main():
+    #First we need to make sure we have the right user and tool frame
+    
+    #Below we create a tool frame to make sure it's oriented correctly. 
+    #Since the tool frame's "z" axis is pointing down by defulat we need to rotate it to point up for what we are going to be doing. 
+    set_tool_frame("tool_frame1", orientation=p[0,0,0,180,0,0]) 
+    change_tool_frame("tool_frame1") #After creating it the tool frame we make sure we are using it.
+    
+    #Make sure you set up a user frame, call it "user_frame1" or any name you wish
+    #Learn how to set up a user frame here: https://www.youtube.com/watch?v=HHvmWXkA0xs
+    change_user_frame("user_frame1")
+
+    numOfTriangles = 50 # Number of triangles
+    angleOffset = math.radians(360/numOfTriangles) #angle offset 
+    v_scl = 100 # line scale
+    curr_angle = 0 # current angle 
+    
+    homeOffset_x = 100
+    homeOffset_y = 100
+    x = 0
+    y = 0
+
+    home_point = p[homeOffset_x, homeOffset_y, 0, 0, 0, 0]
+    for i in range(numOfTriangles):
+        '''
+        NOTE: Below you will notice that we used movej() instead of movel() and yet
+        the example was supposed to be talking about movel().
+        The point is that it's good practice to run movej() as your first move commande 
+        for the very first time you ran your program depending on what you are planning to do.
+        The reason is that movel() is a linear command and movej() is a joint move commnad. 
+        If the robot's joints were previously in a weird orientaion that locks its movements, a linear command might not work. 
+        So joint move cammand (movej()) can be used to reorient the joints.
+        '''
+        #For each triangle we draw...
+        movej(home_point) # Move to home point
+        # Using the current angle calculate the 2nd point of the triangle...
+        x = math.cos(curr_angle)*v_scl
+        y = math.sin(curr_angle)*v_scl
+        # Adjust the current (x, y) point to be relative to the home point by adding the home offset to it
+        x = x + homeOffset_x
+        y = y + homeOffset_y
+        movel(p[x, y, 0, 0, 0, 0 ]) # Move to the 2nd point
+   	    
+        # calculate the 3rd point of the triangle...
+        temp_angle = curr_angle + math.radians(30)# Create a temporary angle offset 30degs from the current angle 
+        # Use the temp angle to Calculate the 3rd point of the triangle
+        next_x = math.cos(temp_angle)*v_scl/2
+        next_y = math.sin(temp_angle)*v_scl/2
+        
+        # Adjust the next (x, y) point to be relative to the home point by adding the home offset to it
+        next_x = next_x + homeOffset_x
+        next_y = next_y + homeOffset_y
+        movel(p[next_x, next_y, 0, 0, 0, 0 ]) # Move to the 3rd point of the triangle
+
+   	    # Adjust the current angle by adding an offset (this will be used to draw the next triangle away from the previous one)
+        curr_angle = curr_angle + angleOffset
+        movel(home_point) #Move to a home point
+    
+    exit() #Exit/stop the main() loop when finished
