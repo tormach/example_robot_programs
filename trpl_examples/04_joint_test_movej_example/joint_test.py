921d857b72f924c0807198c859eabe2b01bdbf5b
zick2 <34427350+zick2@users.noreply.github.com>
2023-02-06 10:30:05 -0600

    adding comments and refactoring code

---
+++ trpl_examples/04_joint_test_movej_example/joint_test.py
@@ -0,0 +1,50 @@
+'''
+Example 4: movej() and Joints()/j[]
+This example will show you how to use Joints() waypoints with
+movej() by going through each joint, rotating it from 0 to 20 degrees and back to 0.
+'''
+
+from robot_command.rpl import *
+
+
+# Set your units
+set_units("mm", "deg")
+
+# The main function runs in a loop
+def main():
+    angle = 20
+
+    notify("Make sure the environment is clear", warning=True)  # Show a notification popup and set warning to true so that the program poses till "OK" is clicked
+    movej(j[0, 0, 0, 0, 0, 0])  # Move all joints to angle zero
+
+    # j1
+    notify("Joint 1/ j1", warning=True)
+    movej(j[angle, 0, 0, 0, 0, 0])  # Move joint 1 to given angle
+    movej(j[0, 0, 0, 0, 0, 0])  # Move back to zero
+
+    # j2
+    notify("Joint 2/ j2", warning=True)
+    movej(j[0, angle, 0, 0, 0, 0])  # Move joint 2 to given angle
+    movej(j[0, 0, 0, 0, 0, 0])   # Move back to zero
+
+    # j3
+    notify("Joint 3/ j3", warning=True)
+    movej(Joints(j3=angle))  # Move joint 3 to given angle
+    movej(Joints())   # Move back to zero
+
+    # j4
+    notify("Joint 4/ j4", warning=True)
+    movej(Joints(j4=angle))  # Move joint 4 to given angle
+    movej(Joints())   # Move back to zero
+
+    # j5
+    notify("Joint 5/ j5", warning=True)
+    movej(Joints(j4=angle))  # Move joint 5 to given angle
+    movej(Joints())   # Move back to zero
+
+    # j6
+    notify("Joint 6/ j6", warning=True)
+    movej(Joints(j6=angle))  # Move joint 6 to given angle
+    movej(Joints())   # Move back to zero
+
+    exit()  # Exit/stop main() loop
---
+++ trpl_examples/05_difference_between_movej_and_movel/difference_between_movej_and_movel.py
@@ -0,0 +1,22 @@
+'''
+Example 5: movej(), movel() and Joints()/j[]
+This example demonstrates the difference between a joint move and linear move.
+'''
+
+from robot_command.rpl import *
+
+
+#  Set units
+set_units("mm", "deg")
+
+# The main function runs in a loop
+def main():
+    # Joint moves
+    movej(j[-50, 0, 0, 0, 10, 0])   # Rotate joint 1 to -50 degrees and joint 5 to 10 degrees
+    movej(j[50, 0, 0, 0, 10, 0])  # Rotate joint 1 to  50 degrees and joint 5 to 10 degrees
+    movej(j[-50, 0, 0, 0, 10, 0])  # Rotate joint 1 to  -50 degrees and joint 5 to 10 degrees
+
+    # linear moves
+    movel(j[50, 0, 0, 0, 10, 0])  # Move to a waypoint with joint 1 rotated 50 degrees  and joint 5 rotated 10 degrees
+    movel(j[-50, 0, 0, 0, 10, 0])   # Move to a waypoint with joint 1 rotated -50 degrees  and joint 5 rotated 10 degrees
+    movel(j[50, 0, 0, 0, 10, 0])   # Move to a waypoint with joint 1 rotated 50 degrees  and joint 5 rotated 10 degrees
