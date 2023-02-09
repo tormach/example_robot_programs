e18adc0d7e529834c01908952436b70bc5035248
zick2 <34427350+zick2@users.noreply.github.com>
2023-02-09 12:07:39 -0600

    refactor: making angle variable global and renaming files

--- trpl_examples/04_joint_test_movej_example/joint_test.py
+++ trpl_examples/04_joint_test_movej_example/joint_test.py
@@ -6,46 +6,44 @@ movej() by going through each joint, rotating it from 0 to 20 degrees and back t
 
 from robot_command.rpl import *
 
-
+ANGLE = 20
 # Set your units
 set_units("mm", "deg")
 
 # The main function runs in a loop
 def main():
-    angle = 20
-
     notify("Make sure the environment is clear", warning=True)  # Show a notification popup and set warning to true so that the program poses till "OK" is clicked
     movej(j[0, 0, 0, 0, 0, 0])  # Move all joints to angle zero
 
     # j1
     notify("Joint 1/ j1", warning=True)
-    movej(j[angle, 0, 0, 0, 0, 0])  # Move joint 1 to given angle
+    movej(j[ANGLE, 0, 0, 0, 0, 0])  # Move joint 1 to given angle
     movej(j[0, 0, 0, 0, 0, 0])  # Move back to zero
 
     # j2
     notify("Joint 2/ j2", warning=True)
-    movej(j[0, angle, 0, 0, 0, 0])  # Move joint 2 to given angle
+    movej(j[0, ANGLE, 0, 0, 0, 0])  # Move joint 2 to given angle
     movej(j[0, 0, 0, 0, 0, 0])   # Move back to zero
 
     # j3
     notify("Joint 3/ j3", warning=True)
-    # Using Joints() function you can specify which joint to move e.g Joints(j3=angle)
-    movej(Joints(j3=angle))  # Move joint 3 to given angle
+    # Using Joints() function you can specify which joint to move e.g Joints(j3 = angle)
+    movej(Joints(j3=ANGLE))  # Move joint 3 to given angle
     movej(Joints())   # Move back to zero. Un empty Joints() will default all joint angles to zero
 
     # j4
     notify("Joint 4/ j4", warning=True)
-    movej(Joints(j4=angle))  # Move joint 4 to given angle
+    movej(Joints(j4=ANGLE))  # Move joint 4 to given angle
     movej(Joints())   # Move back to zero
 
     # j5
     notify("Joint 5/ j5", warning=True)
-    movej(Joints(j4=angle))  # Move joint 5 to given angle
+    movej(Joints(j4=ANGLE))  # Move joint 5 to given angle
     movej(Joints())   # Move back to zero
 
     # j6
     notify("Joint 6/ j6", warning=True)
-    movej(Joints(j6=angle))  # Move joint 6 to given angle
+    movej(Joints(j6=ANGLE))  # Move joint 6 to given angle
     movej(Joints())   # Move back to zero
 
     exit()  # Exit/stop main() loop
---
+++ trpl_examples/05_difference_between_movej_and_movel_using_joint_waypoints/README.md
@@ -0,0 +1,15 @@
+# Example 5: difference_between_movej_and_movel
+In this example we show the difference between `movej()` and `movel()` using `Joints()/j[]` waypoints.
+The video below demonstrates a joint move and a linear move.
+
+https://user-images.githubusercontent.com/34427350/217036060-2243b740-f347-45e1-a520-37c5661323d6.mp4
+
+In the example we learn about:
+
+* `movej()`: A joint move command. With this command, the robot end effector will move between points using the available efficient path, also considering angle limits for each joint.
+* `movel()`: A linear move command. With this command, a robot end effector will move between points linearly.
+* `Joints()`: This is used to create joint waypoints and its short form is j[].
+* `set_units()`: Set units that will be used in a program
+
+### Learn more about the Tormach Robot Programming Language (TRPL):
+https://tormach.atlassian.net/wiki/spaces/ROBO/pages/1930690719/Tormach+Robot+Programming+Language#TormachRobotProgrammingLanguage-robot_command.rpl.execute_trajectory
