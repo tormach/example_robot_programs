665245b44a83bcfb653c76ad0e5062aa9b1624d8
zick2 <34427350+zick2@users.noreply.github.com>
2023-03-23 10:58:42 -0500

    Update pause_example.py

--- trpl_examples/08_sleep_and_pause/pause_example.py
+++ trpl_examples/08_sleep_and_pause/pause_example.py
@@ -1,11 +1,8 @@
 '''
 Example 8: Pause example
 '''
-
 from robot_command.rpl import *
-
 set_units("mm", "deg")  # set units
-
 # A function to move in a rectangular formation
 def draw_rect(x=0, y=0, w=100, h=100):
     movej(p[x,y,0,0,0,0])
@@ -13,19 +10,18 @@ def draw_rect(x=0, y=0, w=100, h=100):
     movel(p[x+w,y+h,0,0,0,0])
     movel(p[x,y+h,0,0,0,0])
 
-
 def main():
     '''
     pause() is different form sleep() function in that it requires user input from the system UI to resume the program.
     Also you can make a pause optional by setting the "optional" parameter to true (pause(optional=True)).
     Optional pauses will be skipped unless you activate them by clicking "OPTIONAL PAUSE" button in the system UI.
     '''
-    # Draw a rectangle two times
+    notify("This program demonstrates the pause() command.  It draws one rectangle, then pauses, waiting for cycle start to resume.", warning=True)
     draw_rect()
-    draw_rect()
-    notify("The program should be pause by the time you see this popup, click \"CYCLE START\" to resume")
+    notify("The program is now paused, click \"CYCLE START\" to resume")
     pause()  # Once the program reaches this point it will be paused till you click "CYCLE START" again to resume the program
-
+    draw_rect()  # Draw a rectangle
+    notify("The robot will draw two more rectangles, with an optional pause in between.  If \"OPTIONAL PAUSE\" is enabled the program will pause, otherwise it will draw both rectangles immediately.", warning=True)
     draw_rect()  # Draw a rectangle
     pause(optional=True)    # This pause will be skipped (activate it by clicking "OPTIONAL PAUSE" in the system UI)
     draw_rect()  # Draw a rectangle
