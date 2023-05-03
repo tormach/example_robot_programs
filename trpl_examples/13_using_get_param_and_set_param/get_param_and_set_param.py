f3506bad25809ab68355049fb8e24ac1129849c3
zick2 <34427350+zick2@users.noreply.github.com>
2023-05-03 11:03:48 -0500

    Update get_param_and_set_param.py

--- trpl_examples/13_using_get_param_and_set_param/get_param_and_set_param.py
+++ trpl_examples/13_using_get_param_and_set_param/get_param_and_set_param.py
@@ -35,39 +35,36 @@ def setup_test_user_and_tool_frame():
 def main():
     setup_test_user_and_tool_frame()
 
-    notify("This example will be showing you how to use get_/set_param().\nAt the beginning of the program we will try getting the saved waypoints, if there is any.\nIf nothing was found we will create the way points and save them.", warning=True)
+    notify("This example will be showing you how to use get_/set_param().\nAt the beginning of the program we will try getting the saved waypoints, if there is any.\nIf nothing was found we will create new ones and save them.", warning=True)
     # Getting data that was stored, if nothing was stored, "None" will be returned by default.
     waypoint_1 = get_param("wp_1")
     waypoint_2 = get_param("wp_2")
     waypoint_3 = get_param("wp_3")
-   
-	# Check if retrieved data exists or is empty
-    if (waypoint_1 == None and waypoint_2 == None and waypoint_3 == None): # If nothing was saved ...
-        
-        notify("No waypoints were found, so we will create new ones and save them.", warning=True)
 
+    # Check if retrieved data exists or is empty
+    if (waypoint_1 == None and waypoint_2 == None and waypoint_3 == None): # If nothing was saved ...
         # Create and save waypoints, using there corresponding names as keys which we will use to access them.
         set_param("wp_1", Pose())
         set_param("wp_2",  Pose(x=100))
         set_param("wp_3",  Pose(y=100))
-        
+
         # Getting the saved waypoints
         waypoint_1 = get_param("wp_1")
         waypoint_2 = get_param("wp_2")
         waypoint_3 = get_param("wp_3")
-        
-        notify("Waypoints have been created and saved.\nClick \"OK\" to draw them", warning=True)
-    else:
-        notify("It looks like you saved some waypoints.\n\nThe saved waypoints have been retrieved and are ready to be ran:\n\nwaypoint_1:\n"+str(waypoint_1)+"\n\nwaypoint_2:\n"+str(waypoint_2)+"\n\nwaypoint_3:\n"+str(waypoint_3) +"\n\n Click \"OK\" to draw the waypoints", warning=True)
 
-	# Draw waypoints
+        notify("No waypoints were found, so new ones have been created and saved.\nClick \"OK\" to draw them", warning=True)
+    else:
+        notify("It looks like there were  some saved waypoints.\n\nThe saved waypoints have been retrieved and are ready to be ran:\n\nwaypoint_1:\n"+str(waypoint_1)+"\n\nwaypoint_2:\n"+str(waypoint_2)+"\n\nwaypoint_3:\n"+str(waypoint_3) +"\n\n Click \"OK\" to draw them", warning=True)
+
+    # Draw waypoints
     movej(waypoint_1)
     movel(waypoint_2)
     movel(waypoint_3)
     movel(waypoint_1)
-    
-	# Handling data deletion ...
-    delete_stored_data = input("The stored data will be persistent, meaning you will still be able to access it even after you stop/abort the program and run it again. You can delete that data by calling the delete_param() function.\n\n Enter \"yes\" to delete the data.",default="no" )
+
+    # Handling data deletion ...
+    delete_stored_data = input("The stored data will be persistent, meaning you will still be able to access it even after you stop/abort the program. You can delete that data by calling the delete_param() function.\n\n If you want to delete the saved waypoints enter \"yes\".",default="no" )
     if (delete_stored_data == "yes" or delete_stored_data == "Yes"):
         # Delete saved data
         delete_param("wp_1")
